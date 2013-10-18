bc0b8daff64a8699b2c3545d1785a557e6baeb4e
kazami <kazamidoly@gmail.com>
2013-10-18 13:04:01 +0900

    Add .gitconfig

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ mkdir -pv ${HOME}/.vim/
 mkdir -pv ${HOME}/.vnc/
 
 ln -sv ${HOME}/.dotfiles/.tmux.conf ${HOME}/.tmux.conf
+ln -sv ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
 ln -sv ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
 ln -sv ${HOME}/.dotfiles/.vim/colors/ ${HOME}/.vim/colors
 ln -sv ${HOME}/.dotfiles/.vnc/xstartup ${HOME}/.vnc/xstartup
